11a89ab0b3611648c8b3bd63f7c7ddab3229734c
educeciliato <148019576+educeciliato@users.noreply.github.com>
2025-06-29 19:19:43 -0300

    Update Ex1.java

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,27 +1,22 @@
-import java.util.*;
-
 public class Ex1 {
     public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        System.out.println("Passos do Bubble Sort:");
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
-            System.out.println("Passo " + (i + 1) + ": " + Arrays.toString(arr));
+            System.out.print("Passo " + (i + 1) + ": ");
+            exibirVetor(arr);
         }
     }
 
     public static void selectionSort(int[] arr) {
-        int n = arr.length;
-        System.out.println("Passos do Selection Sort:");
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
@@ -29,14 +24,14 @@ public class Ex1 {
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
-            System.out.println("Passo " + (i + 1) + ": " + Arrays.toString(arr));
+
+            System.out.print("Passo " + (i + 1) + ": ");
+            exibirVetor(arr);
         }
     }
 
     public static void insertionSort(int[] arr) {
-        int n = arr.length;
-        System.out.println("Passos do Insertion Sort:");
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < arr.length; i++) {
             int chave = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] > chave) {
@@ -44,73 +39,30 @@ public class Ex1 {
                 j--;
             }
             arr[j + 1] = chave;
-            System.out.println("Passo " + i + ": " + Arrays.toString(arr));
+
+            System.out.print("Passo " + i + ": ");
+            exibirVetor(arr);
         }
     }
 
-    public static void mergeSort(int[] arr) {
-        System.out.println("Passos do Merge Sort:");
-        mergeSortHelper(arr, 0, arr.length - 1);
-    }
-
-    private static void mergeSortHelper(int[] arr, int esquerda, int direita) {
-        if (esquerda < direita) {
-            int meio = esquerda + (direita - esquerda) / 2;
-            mergeSortHelper(arr, esquerda, meio);
-            mergeSortHelper(arr, meio + 1, direita);
-            merge(arr, esquerda, meio, direita);
+    public static void exibirVetor(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1)
+                System.out.print(", ");
         }
-    }
-
-    private static void merge(int[] arr, int esquerda, int meio, int direita) {
-        int n1 = meio - esquerda + 1;
-        int n2 = direita - meio;
-
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        System.arraycopy(arr, esquerda, L, 0, n1);
-        System.arraycopy(arr, meio + 1, R, 0, n2);
-        int i = 0, j = 0;
-        int k = esquerda;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-        System.out.println("Mesclando: " + Arrays.toString(arr));
+        System.out.println("]");
     }
 
     public static void main(String[] args) {
         int[] numeros = { 3, 4, 9, 2, 5, 8, 2, 1, 7 };
 
-        System.out.println("Array Original: " + Arrays.toString(numeros));
+        System.out.print("Array Original: ");
+        exibirVetor(numeros);
 
-        int[] bubbleArr = numeros.clone();
-        bubbleSort(bubbleArr);
-
-        int[] selectionArr = numeros.clone();
-        selectionSort(selectionArr);
-
-        int[] insertionArr = numeros.clone();
-        insertionSort(insertionArr);
-
-        int[] mergeArr = numeros.clone();
-        mergeSort(mergeArr);
+        bubbleSort(numeros.clone());
+        selectionSort(numeros.clone());
+        insertionSort(numeros.clone());
     }
 }
